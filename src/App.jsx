f386218677962df9b7293b743fe7524a79df1f63
SarasiRajarathna <rajarathnasarasi@gmail.com>
2025-09-04 22:50:49 +0530

    Modified Navbar and create Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import BodyContent from "./Components/BodyContent/BodyContent"
 import FooterContent from "./Components/FooterContent/FooterContent"
 import HeaderContent from "./Components/HeaderContent/HeaderContent"
+import Hero from "./Components/HeroSection/Hero"
 
 function App() {
   return (
     <>
      <div id="wrapper">
       <HeaderContent/>
+      <Hero/>
       <BodyContent/>
       <FooterContent/>
      </div>
--- src/Components/HeaderContent/HeaderContent.jsx
+++ src/Components/HeaderContent/HeaderContent.jsx
@@ -3,24 +3,26 @@ import MenuLink from '../MenuLink/MenuLink'
 
 function HeaderContent() {
   return (
-    <div id='navcontent' className='bg-black text-white p-4 flex items-center justify-between'>
+    <div id='navcontent' className='bg-black text-white p-4 flex items-center justify-between ml-28'>
         <a href="#logo" className="text-2xl font-bold">
           <img src="" alt="" /> </a>
 
+      <div className='flex-1 flex justify-center'>
         <div className='flex space-x-10'>
             <MenuLink linkname="Home" url="#Home"/>
-            <MenuLink linkname="About" url="#About"/>
+            <MenuLink linkname="About Me" url="#About Me"/>
             <MenuLink linkname="Projects" url="#Projects"/>
             <MenuLink linkname="Skills" url="#Skills"/>
             <MenuLink linkname="Contact" url="#Contact"/>
-
+        </div>
+      </div>  
             <a
              href="#contact"
-              className="ml-4 bg-amber-400 hover:bg-neutral-400 text-white px-4 py-2 rounded-lg shadow-md transition duration-300"
+              className="mr-4 bg-gradient-to-r from-fuchsia-600 to-orange-500 hover:scale-105 text-white px-4 py-2 rounded-3xl shadow-md transition duration-300"
               >
-             Hire Me
+             Connect With Me
             </a>
-        </div>
+        
     </div>
   )
 }
